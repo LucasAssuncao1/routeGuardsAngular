10ea3421b6ec9445c00e10c75c1ec9f0554f2875
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-19 19:50:52 -0300

    criando components e o activeGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+import { canActiveGuard } from './shared/guards/can-active.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'account',
+        component: AccountComponent,
+        canActivate: [canActiveGuard]
+    },
+];
